7ae81521e8ae1ffff3327c40071aa4f7d9877d13
Adrian Tobi <40503266+Adriantobi@users.noreply.github.com>
2023-10-27 18:42:01 +0100

    Style: Gap Form Input Label and Input

--- components/FormInput.js
+++ components/FormInput.js
@@ -5,8 +5,8 @@ export default function FormInput({title, placeholder, required, textbox, type})
     <div className={styles.FormInput}>
         <label for={title} className={styles.label}>{title} {required ? "*" : ""}</label>
         {textbox ? 
-            <textarea className={styles.textarea} name={title} placeholder={placeholder} maxLength='5000' id={title} {...(required ? required : "")} /> : 
-            <input className={styles.input} id={title} name={title} type={type ? type : 'text'} maxLength='256' placeholder={placeholder} {...(required ? required : "")} />
+          <textarea className={styles.textarea} name={title} placeholder={placeholder} maxLength='5000' id={title} {...(required ? required : "")} /> : 
+          <input className={styles.input} id={title} name={title} type={type ? type : 'text'} maxLength='256' placeholder={placeholder} {...(required ? required : "")} />
         }
     </div>
   )
